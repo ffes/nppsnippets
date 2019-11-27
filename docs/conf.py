b5e34bc5fcd05dd7b093cfed1cadc88f4d5bfd58
Frank Fesevur <fesevur@gmail.com>
2019-11-27 16:17:27 +0100

    Fix generation of docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 # serve to show the default.
 
 import sys, os
+from datetime import datetime
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
