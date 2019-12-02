dd1317bbac3a0831d083eed99502c663d34b8f95
Frank Fesevur <fesevur@gmail.com>
2019-12-02 21:28:04 +0100

    Modernize batch file

--- version_git.bat
+++ version_git.bat
@@ -1,32 +1,29 @@
 @echo off
 
-REM Is Git for Windows installed?
-if exist "%ProgramFiles%\Git\bin\bash.exe" goto git_for_win
+REM Is Git for Windows x64 installed?
+if exist "%ProgramW6432%\Git\bin\bash.exe" (
+
+	echo Using bash from Git for Windows x64
+	PATH=%ProgramW6432%\Git\mingw64\bin;%ProgramW6432%\Git\bin
+	bash version_git.sh
+	exit /b 0
+)
 
 REM Is Cygwin installed?
-if exist C:\Cygwin\bin\bash.exe goto cygwin
+if exist C:\Cygwin\bin\bash.exe (
+
+	echo Using bash from Cygwin
+	PATH=C:\Cygwin\bin
+	bash version_git.sh
+	exit /b 0
+)
 
 REM No Cygwin or Git for Windows installed
-REM If there is a file assume if is propperly generated
-if exist version_git.h goto end
+REM If there is a file assume if is properly generated
+if exist version_git.h (
+	echo No bash found, but version_git.h exists
+	exit /b 0
+)
 
-REM Generate a dummy file
-echo No bash found, generating dummy version_git.h...
-echo #pragma once > version_git.h
-echo #define VERSION_GIT_STR "UNKNOWN" >> version_git.h
-echo #define VERSION_GIT_WSTR L"UNKNOWN" >> version_git.h
-goto end
-
-REM Let a Cygwin bash script do all the work
-:cygwin
-PATH=C:\Cygwin\bin
-bash version_git.sh
-goto end
-
-REM Let a Git for Windows bash script do all the work
-:git_for_win
-PATH=%ProgramFiles%\Git\mingw64\bin;%ProgramFiles%\Git\bin
-bash version_git.sh
-goto end
-
-:end
+echo No bash found
+exit /b 1
