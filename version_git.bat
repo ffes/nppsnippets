fb8f4798edd764decab130cf98c0c50123d62c4c
Frank Fesevur <fesevur@gmail.com>
2020-05-06 08:42:00 +0200

    Detect Cygwin64 to get the version from git

--- version_git.bat
+++ version_git.bat
@@ -4,6 +4,7 @@ REM Is Git for Windows x64 installed?
 if exist "%ProgramW6432%\Git\bin\bash.exe" (
 
 	echo Using bash from Git for Windows x64
+	setlocal
 	PATH=%ProgramW6432%\Git\mingw64\bin;%ProgramW6432%\Git\bin
 	bash version_git.sh
 	exit /b 0
@@ -13,7 +14,18 @@ REM Is Cygwin installed?
 if exist C:\Cygwin\bin\bash.exe (
 
 	echo Using bash from Cygwin
+	setlocal
 	PATH=C:\Cygwin\bin
+	bash version_git.sh
+	exit /b 0
+)
+
+REM Is Cygwin64 installed?
+if exist C:\Cygwin64\bin\bash.exe (
+
+	echo Using bash from Cygwin64
+	setlocal
+	PATH=C:\Cygwin64\bin
 	bash version_git.sh
 	exit /b 0
 )
